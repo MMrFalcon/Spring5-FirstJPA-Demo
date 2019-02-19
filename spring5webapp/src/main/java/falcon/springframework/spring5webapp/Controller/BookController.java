0a4d548a8999242f60da65aa65fdc7f49c35d072
MMrFalcon <kubasokol123456@gmail.com>
2019-02-19 18:33:54 +0100

    Creation of Book Controller, for returning books list.

---
+++ spring5webapp/src/main/java/falcon/springframework/spring5webapp/Controller/BookController.java
@@ -0,0 +1,27 @@
+package falcon.springframework.spring5webapp.Controller;
+
+import falcon.springframework.spring5webapp.Repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private BookRepository bookRepository;
+
+    /**
+     * Constructor Based Dependency Injection - Possible when class have only one constructor
+     * Different method is using @Autowired annotation
+     */
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "books";
+    }
+}
